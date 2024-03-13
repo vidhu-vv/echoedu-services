4b8de18bb48a9f6e31005159997d76707507c4dd
halftone <86031051+halftonenano@users.noreply.github.com>
2024-03-12 20:01:27 -0700

    fixed notification to show the correct timeslot

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-const fs = require("node:fs");
-const path = require("node:path");
-const { Client, Collection, Events, GatewayIntentBits } = require("discord.js");
-const PocketBase = require("pocketbase/cjs");
-const dotenv = require("dotenv");
-const dayjs = require("dayjs");
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const PocketBase = require('pocketbase/cjs');
+const dotenv = require('dotenv');
+const dayjs = require('dayjs');
 dotenv.config();
-const eventsource = require("eventsource");
+const eventsource = require('eventsource');
 global.EventSource = eventsource;
 
-const { sendToNumber, carriers } = require("./notify"); // OMGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
+const { sendToNumber, carriers } = require('./notify'); // OMGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGGG
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 client.commands = new Collection();
 
-const foldersPath = path.join(__dirname, "commands");
+const foldersPath = path.join(__dirname, 'commands');
 const commandFolders = fs.readdirSync(foldersPath);
 
 for (const folder of commandFolders) {
   const commandsPath = path.join(foldersPath, folder);
   const commandFiles = fs
     .readdirSync(commandsPath)
-    .filter((file) => file.endsWith(".js"));
+    .filter((file) => file.endsWith('.js'));
   for (const file of commandFiles) {
     const filePath = path.join(commandsPath, file);
     const command = require(filePath);
-    if ("data" in command && "execute" in command) {
+    if ('data' in command && 'execute' in command) {
       client.commands.set(command.data.name, command);
     } else {
       console.log(
@@ -57,12 +57,12 @@ client.on(Events.InteractionCreate, async (interaction) => {
     console.error(error);
     if (interaction.replied || interaction.deferred) {
       await interaction.followUp({
-        content: "There was an error while executing this command!",
+        content: 'There was an error while executing this command!',
         ephemeral: true,
       });
     } else {
       await interaction.reply({
-        content: "There was an error while executing this command!",
+        content: 'There was an error while executing this command!',
         ephemeral: true,
       });
     }
@@ -77,61 +77,67 @@ client.login(TOKEN);
 //            |
 // pocketbase v
 
-const pb = new PocketBase("https://api.echo-edu.org");
-
+const pb = new PocketBase('https://api.echo-edu.org');
 
 (async () => {
   await pb.admins.authWithPassword(
     process.env.API_ADMIN_EMAIL,
     process.env.API_ADMIN_PASSWORD
-    );
-    
-    await pb.collection('applications').subscribe('*', function (e) {
-      if(e.action === "create"){
+  );
+
+  await pb.collection('applications').subscribe(
+    '*',
+    function (e) {
+      if (e.action === 'create') {
         client.channels.cache
-        .get(CHANNEL_ID)
-        .send(
-          `For <@${DISCORD_ADMIN_ID}>\nNew application: ${e.record.expand.user.name} - ${e.record.expand.user.id}`
-        );
-      }  
-    }, { expand: "user"});
-    routine();
-    
-    setInterval(routine, 5000);
-  })();
-  
+          .get(CHANNEL_ID)
+          .send(
+            `For <@${DISCORD_ADMIN_ID}>\nNew application: ${e.record.expand.user.name} - ${e.record.expand.user.id}`
+          );
+      }
+    },
+    { expand: 'user' }
+  );
+  routine();
+
+  setInterval(routine, 5000);
+})();
+
 async function routine() {
-  const resultList = await pb.collection("notifications").getFullList({
-    filter: "datetime <= @now",
-    expand: "session,phone,session.tutee,session.tutor, session.datetime",
+  const resultList = await pb.collection('notifications').getFullList({
+    filter: 'datetime <= @now',
+    expand: 'session,phone,session.tutee,session.tutor, session.datetime',
   });
   for (const result of resultList) {
     const { reason } = result;
 
-
     const { session } = result.expand;
     if (!session) {
       console.log(`session not found for ${result.id}`);
-      await pb.collection("notifications").delete(result.id);
+      await pb.collection('notifications').delete(result.id);
       continue;
     }
     const { tutee, tutor } = session.expand;
-    const formattedTime = dayjs(session.datetime, "America/Los_Angeles").format(
-      "dddd, MMMM D"
+    const formattedTime = dayjs(session.datetime, 'America/Los_Angeles').format(
+      'dddd, MMMM D'
     );
 
+    const hour = dayjs(session.datetime).hour();
+    const timeslot =
+      hour <= 11 ? 'at TUTORIAL' : hour <= 15 ? 'DURING 7th' : 'AFTER 7th';
+
     const message = {
-      "booking/new": `You have a new booking for ${formattedTime} at TUTORIAL with ${
-        result.tutee ? tutor.name : tutee ? tutee.name : "the student"
+      'booking/new': `You have a new booking for ${formattedTime} ${timeslot} with ${
+        result.tutee ? tutor.name : tutee ? tutee.name : 'the student'
       }`,
-      "booking/canceled": `Your booking for ${formattedTime} has been cancelled by ${
-        result.tutee ? tutor.name : tutee ? tutee.name : "the student"
+      'booking/canceled': `Your booking for ${formattedTime} has been cancelled by ${
+        result.tutee ? tutor.name : tutee ? tutee.name : 'the student'
       }`,
-      "reminder/5mins": `You have a tutoring session in 5 minutes with ${
-        result.tutee ? tutor.name : tutee ? tutee.name : "the student"
+      'reminder/5mins': `You have a tutoring session in 5 minutes with ${
+        result.tutee ? tutor.name : tutee ? tutee.name : 'the student'
       } Location: ${session.location}`,
-      "reminder/morning": `You have a tutoring session today at TUTORIAL with ${
-        result.tutee ? tutor.name : tutee ? tutee.name : "the student"
+      'reminder/morning': `You have a tutoring session today ${timeslot} with ${
+        result.tutee ? tutor.name : tutee ? tutee.name : 'the student'
       } Location: ${session.location}`,
     };
     const { number, carrier } = result.expand.phone;
@@ -142,6 +148,6 @@ async function routine() {
       .send(
         `Sending... "${message[reason]}" to: ${number}${carriers[carrier]}`
       );
-    await pb.collection("notifications").delete(result.id);
+    await pb.collection('notifications').delete(result.id);
   }
 }

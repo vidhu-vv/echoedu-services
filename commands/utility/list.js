738c0b41a82047e0d2e6c7c2c444f06351448b3b
halftone <86031051+halftonenano@users.noreply.github.com>
2024-03-13 15:13:40 -0700

    send user and tutor lists in batches to avoid character limit

--- commands/utility/list.js
+++ commands/utility/list.js
@@ -1,44 +1,61 @@
 const { SlashCommandBuilder } = require('discord.js');
-const PocketBase = require("pocketbase/cjs");
-const dotenv = require("dotenv");
+const PocketBase = require('pocketbase/cjs');
+const dotenv = require('dotenv');
 
 dotenv.config();
 
-const pb = new PocketBase("https://api.echo-edu.org");
+const pb = new PocketBase('https://api.echo-edu.org');
 
 module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('list')
-        .setDescription('lists all tutors or users alphabetically by name')
-        .addBooleanOption((option) =>
-          option
-            .setName("tutors")
-            .setDescription("True: list tutors, False: list users")
-            .setRequired(true)
-        ),
-    async execute(interaction) {
-        if (!pb.authStore.model) {
-            await pb.admins.authWithPassword(
-              process.env.API_ADMIN_EMAIL,
-              process.env.API_ADMIN_PASSWORD
-            );
-          }
-        const isTutors = interaction.options.getBoolean("tutors", true);
-        const sent = await interaction.reply({content: 'Awaiting database response...', fetchReply: true});
-        const tutors = await pb.collection('tutors').getFullList({
-            sort: "name"
-        });
-        const users = await pb.collection('users').getFullList({
-            sort: "name"
-        });
-        const tutorNames = tutors.map(tutor => tutor.name + " - (" + tutor.id + ")").join(',\n')
-        if(!isTutors) {
-            interaction.editReply(`Users: \n${users.map(user => user.name).join(',\n')}`);
-            return;
-        }
-        else {
-          interaction.editReply(`Tutors: \n${tutorNames}`);
-          return;
-        }
+  data: new SlashCommandBuilder()
+    .setName('list')
+    .setDescription('lists all tutors or users alphabetically by name')
+    .addBooleanOption((option) =>
+      option
+        .setName('tutors')
+        .setDescription('True: list tutors, False: list users')
+        .setRequired(true)
+    ),
+  async execute(interaction) {
+    if (!pb.authStore.model) {
+      await pb.admins.authWithPassword(
+        process.env.API_ADMIN_EMAIL,
+        process.env.API_ADMIN_PASSWORD
+      );
     }
+
+    const isTutors = interaction.options.getBoolean('tutors', true);
+    await interaction.deferReply();
+
+    if (isTutors) {
+      const tutors = await pb.collection('tutors').getFullList({
+        sort: 'name',
+      });
+
+      interaction.editReply(`Tutors: (${tutors.length})`);
+      sendInBatches(interaction, tutors);
+    } else {
+      const users = await pb.collection('users').getFullList({
+        sort: 'name',
+      });
+
+      interaction.editReply(`Users: (${users.length})`);
+      sendInBatches(interaction, users);
+    }
+  },
 };
+
+const BATCH_SIZE = 35;
+
+async function sendInBatches(interaction, array) {
+  for (let i = 0; i < Math.ceil(array.length / BATCH_SIZE); i++) {
+    let sendString = '';
+    for (let k = 0; k < BATCH_SIZE; k++) {
+      const item = array[i * BATCH_SIZE + k];
+      if (item) {
+        sendString += `${item.name} \`${item.id}\`\n`;
+      }
+    }
+    await interaction.channel?.send(sendString);
+  }
+}

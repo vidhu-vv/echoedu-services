ec36f9af82b3881a3f8e8d05522ebd641a4a1798
halftone <86031051+halftonenano@users.noreply.github.com>
2024-10-14 16:22:20 -0700

    replaced resend with gmail

--- utils/notify.js
+++ utils/notify.js
@@ -1,41 +1,52 @@
-const { Resend } = require("resend");
+const nodemailer = require('nodemailer');
 
-const resend = new Resend(process.env.RESEND_KEY);
+let transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASS,
+  },
+});
 
 const carriers = {
-  att: "@txt.att.net",
-  tmobile: "@tmomail.net",
-  verizon: "@vtext.com",
-  sprint: "@messaging.sprintpcs.com",
-  boost: "@myboostmobile.com",
-  cricket: "@sms.mycricket.com",
-  metropcs: "@mymetropcs.com",
-  tracfone: "@mmst5.tracfone.com",
-  uscellular: "@email.us",
-  virgin: "@vmobl.com",
-  googlefi: "@msg.fi.google.com",
-  alltel: "@sms.alltelwireless.com",
-  consumer: "@mailmymobile.net",
-  republic: "@text.republicwireless.com",
-  ting: "@message.ting.com",
-  uscellular: "@email.uscc.net",
-  mint: "@tmomail.net",
-  xfinity: "@vtext.com",
+  att: '@txt.att.net',
+  tmobile: '@tmomail.net',
+  verizon: '@vtext.com',
+  sprint: '@messaging.sprintpcs.com',
+  boost: '@myboostmobile.com',
+  cricket: '@sms.mycricket.com',
+  metropcs: '@mymetropcs.com',
+  tracfone: '@mmst5.tracfone.com',
+  uscellular: '@email.us',
+  virgin: '@vmobl.com',
+  googlefi: '@msg.fi.google.com',
+  alltel: '@sms.alltelwireless.com',
+  consumer: '@mailmymobile.net',
+  republic: '@text.republicwireless.com',
+  ting: '@message.ting.com',
+  uscellular: '@email.uscc.net',
+  mint: '@tmomail.net',
+  xfinity: '@vtext.com',
 };
 
 module.exports = {
   carriers,
   async sendToNumber(number, carrier, text) {
-    // resend logic
-    const { data, error } = await resend.emails.send({
-      from: "reminders@echo-edu.org",
+    const mailContent = {
+      from: 'reminders@echo-edu.org',
       to: `${number}${carriers[carrier]}`,
-      subject: "EchoEDU / Reminder",
+      subject: 'EchoEDU / Reminder',
       text: `${text}`,
-    });
+    };
 
-    if (error) {
-      return console.error({ error });
-    }
+    transporter.sendMail(mailContent, (error, info) => {
+      if (error) {
+        console.log('Error:', error);
+      } else {
+        console.log('Email sent: ', info.response);
+      }
+    });
   },
 };
